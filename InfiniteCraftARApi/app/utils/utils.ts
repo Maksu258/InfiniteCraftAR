63a087192a80f2436502c0ab326d084749f30198
BenjaminBrehier <benbrehier@gmail.com>
2024-12-29 18:14:37 +0100

    refactor: update generateFusionWord function to use new API endpoint
    - Changed the API endpoint in the generateFusionWord function from neal.fun to infiniteback.org.
    - Simplified the fetch request by removing unnecessary headers and changing the method from POST to GET.
    - Improved error handling to ensure proper response management.

--- InfiniteCraftARApi/app/utils/utils.ts
+++ InfiniteCraftARApi/app/utils/utils.ts
@@ -46,27 +46,15 @@ export function getCommonLabelsSummary(
 }
 
 export async function generateFusionWord(word1: string, word2: string) {
-  const urlInfiniteCraft = `https://neal.fun/api/infinite-craft/pair?first=${word1}&second=${word2}`
-
-  const headers = {
-    'accept': '*/*',
-    'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-    'dnt': '1',
-    'priority': 'u=1, i',
-    'referer': 'https://neal.fun/infinite-craft/',
-    'sec-ch-ua': '"Not?A_Brand";v="99", "Chromium";v="130"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-  }
+  const urlInfiniteCraft = `https://infiniteback.org/pair?first=${word1}&second=${word2}`
 
   try {
-    const response = await fetch(urlInfiniteCraft, { method: 'POST', headers })
+    const response = await fetch(urlInfiniteCraft, { method: 'GET' })
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`)
     }
+
     const data: any = await response.json()
     return data.result
   } catch (error) {
